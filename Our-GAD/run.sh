96fac7be695304cc7ea87e5dc9d7187bac222ee7
Disheng Tang <72563966+DishengTang@users.noreply.github.com>
2022-01-24 14:45:20 +0800

    Update run.sh

--- Our-GAD/run.sh
+++ Our-GAD/run.sh
@@ -1,2 +1,2 @@
 nohup python -u run_GNN.py --epochs 1000 --net SupCL --lambda_encoder gcn --device 1 > yelp_pretrain.log &
-nohup python -u run_GNN.py --epochs 1000 --net SupCL --lambda_encoder gcn --dataset Amazon --device 0 --lr 0.001 > amazon_pretrain.log &
+nohup python -u run_GNN.py --epochs 1000 --net SupCL --lambda_encoder gcn --dataset Amazon --device 0 > amazon_pretrain.log &
